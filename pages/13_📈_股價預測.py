e608973ed97d6f2622a2657ce26a89efaa2cb04e
Tinalex1012 <131650621+Tinalex1012@users.noreply.github.com>
2023-09-20 12:33:20 +0800

    Add files via upload

---
+++ pages/13_📈_股價預測.py
@@ -0,0 +1,161 @@
+import streamlit as st 
+import pandas as pd
+import yfinance as yf
+import datetime
+from matplotlib import pyplot as plt
+import seaborn as sns
+from statsmodels.tsa.stattools import adfuller
+from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
+from statsmodels.tsa.seasonal import seasonal_decompose
+import statsmodels.api as sm
+
+def test_stationarity(timeseries):
+    rolmean=timeseries.rolling(window=12).mean()
+    rolstd=timeseries.rolling(window=12).std()
+    st.markdown("**ADF檢定**")
+    fig=plt.figure(figsize=(12,4))
+    orig=plt.plot(timeseries,color='blue',label='一次差分')
+    mean=plt.plot(rolmean,color='red',label='移動平均')
+    std=plt.plot(rolstd,color='black',label='標準差')
+    plt.legend(loc='best')
+    plt.title('一次差分 移動平均 & 標準差')
+    st.pyplot(fig)
+    
+    # Perform Dickey-Fuller test:
+    # Dickey-Fuller test is used to determine whether a unit root (a feature that can cause 
+    # issues in statistical inference) is present in an autoregressive model.
+    st.text('Results of Dickey-Fuller Test:')
+    dftest=adfuller(timeseries,autolag='AIC')
+    dfoutput=pd.Series(dftest[0:4],index=['ADF統計量','p-value','滯後期數(#Lags)','資料筆數'])
+    for key,value in dftest[4].items():
+        dfoutput['Critical Value(%s)'%key]=value
+    st.text(dfoutput)
+
+    fig=plt.figure(figsize=(12,6),constrained_layout=True)
+    ax1=fig.add_subplot(211)
+    fig=sm.graphics.tsa.plot_acf(timeseries,lags=20,ax=ax1)
+    ax2=fig.add_subplot(212)
+    fig=sm.graphics.tsa.plot_pacf(timeseries,lags=20,ax=ax2)
+    st.pyplot(fig)
+
+    fig, axarr = plt.subplots(4, sharex=True)
+    fig.set_size_inches(12, 6)
+
+st.title("台股股價預測")
+plt.rcParams['font.sans-serif'] = ['Microsoft JhengHei']
+plt.rcParams['axes.unicode_minus'] = False
+
+stock_list = pd.read_excel('./MODEL/stock_list.xlsx')  
+today = datetime.date.today()
+dateStart = (today - datetime.timedelta(days=365*4)).strftime('%Y-%m-%d')    # 載入三年資料
+dateEnd = today.strftime('%Y-%m-%d')  
+target_stock=""
+draw_chk = 0
+
+col1, col2 = st.columns([1, 5])
+with col1:
+    target_stock = f"{st.text_input('台股代號')}"
+    try:
+        download_chk = 0
+        df = yf.download(f'{target_stock}.TW', start=dateStart, end=dateEnd)
+        df_close = df[['Adj Close']].copy()
+        df_close.columns = ['Close']
+        stock_name = stock_list[stock_list['代號']==int(target_stock)]['股票名稱'].values[0]        
+        download_chk = 1
+    except:
+        st.text(f'查無此股票')
+        draw_chk = 0
+    if download_chk == 1:
+        if st.button('股價預測'):
+            draw_chk=1
+
+
+with col2:
+    try:
+        if draw_chk == 1 & download_chk==1:
+            fig = plt.figure(figsize=(8, 5))
+            plt.title(f'{target_stock} {stock_name} 收盤線圖 ')
+            sns.lineplot(x=df_close.index, y='Close', data=df_close )
+            st.pyplot(fig)
+    except:
+         st.text(f'查無此股票')
+
+try:
+    if draw_chk == 1 & download_chk==1 :
+
+        df_diff = pd.DataFrame()
+        df_diff['Adj Close'] = df['Adj Close'].copy()
+        df_diff['diff'] = df['Adj Close'] - df['Adj Close'].shift(1)
+        test_stationarity(df_diff['diff'].dropna(inplace=False))
+
+        date1 = pd.date_range(start=df.index[0], end=df.index[-1])
+        decomp = pd.DataFrame(index=date1)
+        decomp = decomp.join(df)
+        decomp = decomp.fillna(method='ffill')
+
+        s_dc = seasonal_decompose(decomp['Adj Close'], model='additive')
+        decomp['SDC_Seasonal'] = s_dc.seasonal
+        decomp['SDC_Trend'] = s_dc.trend
+        decomp['SDC_Error'] = s_dc.resid
+        decomp['SDC_TS'] = s_dc.trend + s_dc.seasonal
+        st.markdown("**效應分解**")
+        fig=plt.figure(figsize=(12,6),constrained_layout=True)
+        ax1 = fig.add_subplot(411)
+        ax1 = decomp['Adj Close'].plot(ax=ax1, color='b', linestyle='-',title="實際值")
+        ax2 = fig.add_subplot(412)
+        ax2 = pd.Series(data=decomp['SDC_Trend'], index=decomp.index).plot(color='r', linestyle='-', ax=ax2, title="趨勢")
+        ax3 = fig.add_subplot(413)
+        ax3 = pd.Series(data=decomp['SDC_Seasonal'], index=decomp.index).plot(color='g', linestyle='-', ax=ax3, title="週期效應")
+        ax4 = fig.add_subplot(414)
+        ax4 = pd.Series(data=decomp['SDC_Error'], index=decomp.index).plot(color='k', linestyle='-', ax=ax4, title="殘差")
+        st.pyplot(fig)
+
+        st.markdown("**時間序列**") 
+        MSE = (decomp['SDC_Error']**2).sum() / decomp['SDC_Error'].shape[0]
+        for _ in range(3,len(decomp)):
+            if (decomp.iloc[_:_+1,6][0] == decomp.iloc[0:1,6][0])&(decomp.iloc[_+1:_+2,6][0] == decomp.iloc[1:2,6][0]) & (decomp.iloc[_+2:_+3,6][0] == decomp.iloc[2:3,6][0]):
+                period_num = _
+                break
+        df_SARIMAX = df_diff.copy()
+        train=df_SARIMAX[:int(df_SARIMAX.shape[0]*0.9)]
+        test=df_SARIMAX[int(df_SARIMAX.shape[0]*0.9):]
+        mod=sm.tsa.statespace.SARIMAX(train['diff'],trend='n',order=(0,1,1),seasonal_order=(1,1,0,period_num))
+        results=mod.fit()
+        st.text(results.summary())
+        st.markdown(f"**MSE = {MSE}**") 
+        st.markdown("**股價預測**") 
+        tmp = results.predict(start = int(df_SARIMAX.shape[0]*0.9)+1, end= df_SARIMAX.shape[0], dynamic= True).to_list()
+        test['diff_pred'] = tmp
+        test[['diff', 'diff_pred']].plot(figsize=(12, 6))
+        x, x_diff = test['Adj Close'].iloc[:], test['diff_pred'].iloc[:]
+        C=[]
+        for i in range(len(x)-1):
+            c=x[i]+x_diff[i+1]
+            C.append(c)
+        C=[0]*1+C
+        test['pred']=C
+
+        day_list = [(today + datetime.timedelta(days=i)).strftime('%Y-%m-%d') for i in range(1, period_num+1)]
+        pred_df = pd.DataFrame(columns=['Adj Close', 'diff', 'diff_pred','pred'], index = day_list)
+        last_price = test[-1:]['pred'].values[0]
+        tmp1 = results.predict(start = df_SARIMAX.shape[0]+1, end= df_SARIMAX.shape[0]+period_num, dynamic= True).to_list()
+        pred_df['diff_pred']=tmp1[-period_num:]
+        pred_df.iloc[0:1,3] = last_price + pred_df[0:1]['diff_pred'].values[0]
+        for i in range(1,len(pred_df)):
+            pred_df.iloc[i:i+1,3] = pred_df.iloc[i-1]['pred'] + pred_df.iloc[i]['diff_pred']
+        test = pd.concat([test,pred_df],axis=0)
+        test.index = pd.to_datetime(test.index)
+        test.columns = ['實際收盤','diff','diff_pred','預測值']
+        fig_pred = plt.figure(figsize=(12, 6))
+        plt.title(f'{target_stock} {stock_name} 收盤價預測圖')
+        plt.plot(test[period_num:]['預測值'],color='red',label='預測值')
+        plt.plot(test[period_num:]['實際收盤'],color='blue',label='實際收盤')
+        plt.legend(loc='best')
+        st.pyplot(fig_pred)
+
+except:
+    st.text(f'查無此股票')
+
+
+
+
---
+++ pages/14_🎯_彩號預測.py
@@ -0,0 +1,130 @@
+# streamlit run HW03_web_Solution
+
+# 若出現錯誤：TypeError: Descriptors cannot not be created directly. 可執行下列指令：
+# Set PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python
+
+# streamlit user guide
+# https://docs.streamlit.io/en/stable/api.html#display-interactive-widgets
+
+import streamlit as st
+import pandas as pd
+import numpy as np
+import datetime,requests
+import matplotlib.pyplot as plt
+import seaborn as sns
+from bs4 import BeautifulSoup
+
+def download_data(year):
+    url= f'http://www.olo.com.tw/histNo/mainT539.php?cp={year}'
+
+    # 送出要求，並取得回應資料
+    response = requests.post(url)
+    # response.status_code
+    soup = BeautifulSoup(response.text)
+    data = []
+    table = soup.find('table')
+    rows = table.find_all('tr')
+    for row in rows:
+        cols = row.find_all('td')
+        cols = [ele.text.strip() for ele in cols]
+        data.append([ele for ele in cols if ele]) # Get rid of empty values
+    data = data[2:-1]
+    columns = ['no','n1', 'n2', 'n3', 'n4', 'n5', 'Date']
+    df = pd.DataFrame(data, columns=columns)
+    df = df.dropna()
+    df.drop('no',axis=1,inplace=  True)
+    return df
+
+def number_formula(df):
+    cnt1 = np.zeros([5,40,40]).astype(np.uint8)
+    for i in range(df.shape[0]-1):
+        for j in range(5):
+            for k in range(5):
+                cnt1[0,df.iloc[i,j],(df.iloc[i+1,k])] = cnt1[0,df.iloc[i,j],(df.iloc[i+1,k])]+1
+                if i < df.shape[0]-2:
+                    cnt1[1,df.iloc[i,j],(df.iloc[i+2,k])] = cnt1[1,df.iloc[i,j],(df.iloc[i+2,k])]+1
+                if i < df.shape[0]-3:
+                    cnt1[2,df.iloc[i,j],(df.iloc[i+3,k])] = cnt1[2,df.iloc[i,j],(df.iloc[i+3,k])]+1        
+                if i < df.shape[0]-4:
+                    cnt1[3,df.iloc[i,j],(df.iloc[i+4,k])] = cnt1[3,df.iloc[i,j],(df.iloc[i+4,k])]+1   
+                if i < df.shape[0]-5:
+                    cnt1[4,df.iloc[i,j],(df.iloc[i+5,k])] = cnt1[4,df.iloc[i,j],(df.iloc[i+5,k])]+1                                      
+    cnt1[cnt1<2] = 0
+    df_ball=pd.DataFrame(np.array(df).ravel().astype(np.uint8),columns = ['Ball'])
+
+    plt.figure(figsize=(16,8),constrained_layout=True)
+    fig, ax = plt.subplots()
+    # ax = plt.subplot(121)   
+    ax = fig.gca()
+    ax.set_title(f"最近30期落球統計{datetime.datetime.today().strftime('%Y-%m-%d')}")
+    sns.countplot( x='Ball', data=df_ball)
+    for p in ax.patches:
+        ax.annotate(f'\n{p.get_height()}', (p.get_x(), p.get_height()), color='black', size=10)   
+    st.pyplot(fig) 
+    fig, ax = plt.subplots()
+    item_list = []
+    det_ball = pd.DataFrame()
+    df_ball1 = pd.DataFrame(cnt1[0,df.iloc[-1,:].to_list(),:].T,columns=df.iloc[-1,:].to_list())
+    df_ball2 = pd.DataFrame(cnt1[1,df.iloc[-2,:].to_list(),:].T,columns=df.iloc[-2,:].to_list())
+    df_ball3 = pd.DataFrame(cnt1[2,df.iloc[-3,:].to_list(),:].T,columns=df.iloc[-3,:].to_list())
+    df_ball4 = pd.DataFrame(cnt1[3,df.iloc[-4,:].to_list(),:].T,columns=df.iloc[-4,:].to_list())
+    df_ball5 = pd.DataFrame(cnt1[4,df.iloc[-5,:].to_list(),:].T,columns=df.iloc[-5,:].to_list())
+    det_ball = pd.concat([df_ball1,df_ball2,df_ball3,df_ball4,df_ball5],axis=1)
+    det_ball.replace(0,None,inplace=True)
+    det_ball.dropna(how='all',axis=0,inplace = True)
+    # det_ball.dropna(how='all',axis=1,inplace = True)
+    det_ball.replace(np.nan,0,inplace=True)
+    det_ball.astype(np.uint8)
+    # ax = plt.subplot(122)   
+    ax.set_title(f"最近5期拖牌預測下期號碼可能統計{datetime.datetime.today().strftime('%Y-%m-%d')}")
+    ax.set_xlabel("落球號")
+    ax.set_ylabel("彩號")
+    plt.yticks(np.arange(0.5,det_ball.shape[0]+0.5),fontsize=8,rotation=90)    
+
+    sns.heatmap(det_ball, cmap = 'PuBuGn', annot = True, linewidths = 0.5)
+    ax.set_xticklabels(det_ball.columns,fontsize = 8)
+    plt.gca().invert_yaxis()
+    st.pyplot(fig)
+plt.rcParams['font.sans-serif'] = ['Microsoft YaHei']  # 中文字体设置
+plt.rcParams['axes.unicode_minus'] = False
+st.markdown('# 今彩539落球統計')
+today = datetime.datetime.today().strftime('%Y')
+
+df1 = download_data(str(int(today)-1912))
+df2 = download_data(str(int(today)-1911))
+df = pd.concat([df1,df2],ignore_index=True)
+df['Date'] = pd.to_datetime(df['Date'])
+df['Date'] = df['Date'].apply(lambda x: x.strftime('%Y-%m-%d'))
+
+df.set_index('Date',inplace=True)
+df.columns = ['B1','B2','B3','B4','B5']
+df = df.astype(np.uint8)
+i = 100
+df_new = df.iloc[-i:,:]
+df_new = df_new.reset_index(drop=True)
+
+col1, col2 = st.columns(2) 
+with col1:
+    st.dataframe(df)
+with col2:
+    d = st.date_input("依日期查詢開獎號",datetime.datetime.today(),format="YYYY-MM-DD")
+    if str(d) in df.index:
+        st.write(f"開獎號碼:{df.loc[str(d),:].to_list()}")
+        # st.write(f"{df.index} {d}")
+    else:
+        st.write(f"開獎號碼:暫無")
+
+
+df_ball=pd.DataFrame(np.array(df_new).ravel().astype(np.uint8),columns = ['Ball'])
+fig,ax = plt.subplots(figsize=(12,6), dpi=80)
+ax.set_title(f"近100期落球號碼統計{datetime.datetime.today().strftime('%Y-%m-%d')}")
+sns.countplot( x='Ball', data=df_ball)
+for p in ax.patches:
+    ax.annotate(f'\n{p.get_height()}', (p.get_x(), p.get_height()), color='black', size=8)
+st.pyplot(fig)
+
+if st.button('預測下期號碼'):
+    batch_items = 30
+    start_number = 0
+    df_p = df_new.iloc[start_number-batch_items-1:,:]
+    cnt1 = number_formula(df_p)
